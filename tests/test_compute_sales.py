d618ee2daf0329512110cb4c7d15818bfbf13df1
Jorge Zepeda <a01797233@tec.mx>
2026-02-13 00:30:30 -0600

    * add unit test * update flake and pylint checks * add unit test workflow

---
+++ tests/test_compute_sales.py
@@ -0,0 +1,19 @@
+from src.compute_sales import build_price_dictionary, compute_total_sales
+
+
+def test_compute_sales():
+    catalogue = [
+        {"title": "Product1", "price": 10},
+        {"title": "Product2", "price": 20}
+    ]
+
+    sales = [
+        {"Product": "Product1", "Quantity": 2},
+        {"Product": "Product2", "Quantity": 1}
+    ]
+
+    price_dict = build_price_dictionary(catalogue)
+
+    total = compute_total_sales(price_dict, sales)
+
+    assert total == 40
